075542af385c578c461424c0888786b70cee0617
Nathan Tinius <nathan@amplecloud.com>
2024-02-22 14:56:53 -0600

    Making a Guess the number game

--- week_2/numberGuessingGame.js
+++ week_2/numberGuessingGame.js
@@ -1,6 +1,51 @@
 //  Lets make a 'Guess the Number' game in Javascript!
 
-// What will we need?
+//  Time to get an MVP together
+/*  🦊  MVP = A Minimal Viable Product is the base 
+        of every application you have ever used. It
+        represents the minimum number of components
+        needed to make our programs work. 
+*/
+
+//  🛖  Create a Variable to hold our answer.
+
+
+//  🛖  Create a function that asks the user for a guess.
+
+
+//  🛖  Create a check guess function. **Hint use a comparison 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Lets think of some new features for our Guessing Game.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*  How should we determine points? 
     Should we even use points?
