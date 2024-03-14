ca0eb0401f3363cbd43a1ce72f8e1a4aa3cc5f6e
Nathan Tinius <nathan@amplecloud.com>
2024-03-14 16:30:26 -0500

    Briv

--- week_4/src/app.js
+++ week_4/src/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import bodyParser from 'body-parser';
 import hitPointRoutes from './routes/hitPointsRoutes.js';
 
-const NAMESPACE = 'OutSchool_API';
+// Express is a flexible and minimalist web application framework for Node.js that simplifies the process of building robust and scalable web applications.
 const router = express();
 
+// body-parser is a middleware for Node.js that extracts the body of incoming HTTP requests and exposes it in the req.body object.
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 
--- week_4/src/routes/hitPointsRoutes.js
+++ week_4/src/routes/hitPointsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
+import BRIV from '../../data/briv.json'
 
 const router = express.Router();
 
 router.get("", (req, res) => {
-    res.json({name: "Briv", hitPoints: 25, temporaryHitPoints: 0});
+    res.json({name: BRIV.name, hitPoints: BRIV.hitPoints, temporaryHitPoints: BRIV.temporaryHitPoints});
 })
 
 export default router;
